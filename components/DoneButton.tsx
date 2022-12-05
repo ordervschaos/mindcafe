71a388b15c15645b3bcee0db26e4345d51f560bf
ordervschaos <anzal.ansari@gmail.com>
2022-12-05 17:34:31 -0500

    big done button

--- components/DoneButton.tsx
+++ components/DoneButton.tsx
@@ -2,7 +2,7 @@ import { CheckIcon } from '@heroicons/react/20/solid'
 // Get icon nams from: https://unpkg.com/browse/@heroicons/react@2.0.11/24/outline/
 import { createClient } from "@supabase/supabase-js";
 import { useSession } from "@clerk/nextjs";
-import {supabaseClient} from '../utils/supabaseClient'
+import { supabaseClient } from '../utils/supabaseClient'
 import { useEffect, useState } from 'react';
 
 
@@ -12,36 +12,38 @@ import { useEffect, useState } from 'react';
 
 
 
-export default function DoneButton({meal,isDone,setIsDone,eatenDishesCount,setEatenDishesCount}) {
+export default function DoneButton({ meal, isDone, setIsDone, eatenDishesCount, setEatenDishesCount }) {
   const { session } = useSession();
-  
-  
-  
-  async function markDone(){
+
+
+
+  async function markDone() {
     setIsDone(true)
-    setEatenDishesCount(eatenDishesCount+1)
-    const supabase_client= await supabaseClient(session) 
+    setEatenDishesCount(eatenDishesCount + 1)
+    const supabase_client = await supabaseClient(session)
 
 
-    var response=await supabase_client.from("meal").update({ next_dish_index: meal.next_dish_index+1 }).match({ id: meal.id });
+    var response = await supabase_client.from("meal").update({ next_dish_index: meal.next_dish_index + 1 }).match({ id: meal.id });
     console.log("response")
     console.log(response)
   }
 
-  
-  
 
-    
+
+
+
 
   return (
 
-      <button
-        type="button"
-        className="relative inline-flex items-center rounded  bg-white  text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10"
-      >
-        <CheckIcon onClick={markDone} className= {`mr-2 h-5 w-5 ${isDone?"text-pink-400":"text-gray-400"}`} aria-hidden="true" />
+
+    <div>
+      <button className="cursor-pointer w-12 h-12 rounded-full 
+                  bg-white border border-gray-400 hover:bg-red-500 text-white">
+        <span>
+          <CheckIcon onClick={markDone} className={`p-2 ${isDone ? "text-pink-400" : "text-gray-400 hover:text-white"}`} aria-hidden="true" />
+        </span>
       </button>
-     
+    </div>
 
   )
 }
