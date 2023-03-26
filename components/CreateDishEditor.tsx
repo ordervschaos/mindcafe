c0143525433a8edbabcc1469ced08528026c57f3
ordervschaos <anzal.ansari@gmail.com>
2023-03-26 18:46:49 -0400

    more parsers

--- components/CreateDishEditor.tsx
+++ components/CreateDishEditor.tsx
@@ -41,28 +41,74 @@ export default function Editor({ meal,dishesList, setDishesList }) {
     if (savedData.blocks.length == 0) {
       return
     } else {
-      //update the data in supabase
+      console.log(savedData)
+
+      
+      // update the data in supabase
       var newDishes = []
       var first_index=0
-      savedData.blocks.forEach((block, index) => {
-       if(block.data.text.includes('highlight | Location') || index==savedData.blocks.length-1)
-        {
-          var last_index=index
-          if(index==savedData.blocks.length-1)
-          {
-            last_index=index+1
+      var last_index=0
+      var regex
+      // regex = /highlight\s|\sLocation/ //kindle
+      // regex = /[0-9]+\./ // 1. 2. 3.
+      var includeRegexLine=false
+      var isRegexLineAtStartOrEnd='start' 
+      if(regex)
+        savedData.blocks.forEach((block, index) => {
+           
+        if(isRegexLineAtStartOrEnd=="end"){
+          if(block.data.text.match(regex) || index==savedData.blocks.length-1)
+            {
+              console.log("index",index)
+              last_index=index
+              if(index==savedData.blocks.length-1 || includeRegexLine)
+              {
+                last_index=index+1
+              }
+              var blocks_of_dish = savedData.blocks.slice(first_index,last_index)
+              newDishes.push({
+                content: JSON.stringify({
+                  blocks:blocks_of_dish,
+                  time: savedData.time,
+                  version: savedData.version
+                }), 
+                owner_id: session.user.id, meal_id: meal.id
+              })
+              first_index=index+1
           }
-          var blocks_of_dish = savedData.blocks.slice(first_index,last_index)
-          newDishes.push({content: JSON.stringify({
-            blocks:blocks_of_dish,
-            time: savedData.time,
-            version: savedData.version
-          }), owner_id: session.user.id, meal_id: meal.id})
-          first_index=index+1
+        }else{
+          if(block.data.text.match(regex) || index==savedData.blocks.length-1)
+            {
+              console.log("index",index)
+              console.log("text",block.data.text)
+              
+              last_index=index
+              if(index==savedData.blocks.length-1)
+              {
+                last_index=index+1
+              }
+              console.log("first_index",first_index)
+              console.log("last_index",last_index)
+
+              var blocks_of_dish = savedData.blocks.slice(first_index,last_index)
+              console.log("blocks: ",blocks_of_dish)
+              newDishes.push({
+                content: JSON.stringify({
+                  blocks:blocks_of_dish,
+                  time: savedData.time,
+                  version: savedData.version
+                }), 
+                owner_id: session.user.id, meal_id: meal.id
+              })
+              
+              first_index=index
+          }
         }
-      })
+        })
+      else
+        newDishes.push({content: JSON.stringify(savedData), owner_id: session.user.id, meal_id: meal.id})
       const supabase_client = await supabaseClient(session)
-      // var newDish:any={content: JSON.stringify(savedData), owner_id: session.user.id, meal_id: meal.id}
+      var newDish:any={content: JSON.stringify(savedData), owner_id: session.user.id, meal_id: meal.id}
       var dishResponse=await supabase_client
         .from("dish")
         .insert(newDishes);
